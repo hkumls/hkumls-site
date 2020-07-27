a52099adec4043646af686113525af0d74355173
welvin21 <wewe2712@connect.hku.hk>
2020-07-27 22:01:50 +0800

    Complete members data

--- src/components/team/membersData.ts
+++ src/components/team/membersData.ts
@@ -1,10 +1,21 @@
 import placeholder from "../../assets/images/members/profilePlaceholder.png";
 import welvin from "../../assets/images/members/welvin.png";
+import uday from "../../assets/images/members/uday.png";
+import ahsan from "../../assets/images/members/ahsan.png";
+import fernando from "../../assets/images/members/fernando.png";
+import manya from "../../assets/images/members/manya.png";
+import diab from "../../assets/images/members/diab.png";
+import adrio from "../../assets/images/members/adrio.png";
+import uzair from "../../assets/images/members/uzair.png";
+import eehit from "../../assets/images/members/eehit.png";
+import ismail from "../../assets/images/members/ismail.png";
 
 export const membersData = [
   {
     name: "Ahsan Syed",
-    picture: placeholder,
+    picture: ahsan,
+    linkedin: "https://hk.linkedin.com/in/ahsan-syed-930a2014a",
+    github: "https://github.com/ahsan8244",
   },
   {
     name: "Welvin Bun",
@@ -14,26 +25,50 @@ export const membersData = [
   },
   {
     name: "Manya Agarwal",
-    picture: placeholder,
+    picture: manya,
+    linkedin: "https://linkedin.com/in/manyaagarwal",
+    github: "https://github.com/manyaagarwal",
   },
   {
     name: "Fernando Julio",
-    picture: placeholder,
+    picture: fernando,
+    linkedin: "https://www.linkedin.com/in/fernando-julio-cendra/",
+    github: "https://github.com/fcendra",
   },
   {
     name: "Uday Jain",
-    picture: placeholder,
+    picture: uday,
+    linkedin: "https://linkedin.com/in/uday1331",
+    github: "https://github.com/uday1331",
   },
   {
     name: "Diab Haque",
-    picture: placeholder,
+    picture: diab,
+    linkedin: "https://www.linkedin.com/in/diabhaque/",
+    github: "https://github.com/diabhaque",
   },
   {
     name: "Uzair Bin C. Asim",
-    picture: placeholder,
+    picture: uzair,
+    linkedin: "https://www.linkedin.com/mwlite/in/uzair-bin-asim-6b6225183",
+    github: "https://github.com/Uzair05",
   },
   {
     name: "Ismail Abdul",
-    picture: placeholder,
+    picture: ismail,
+    linkedin: "https://linkedin.com/in/ismail-abdul-rehman-142546173",
+    github: "https://github.com/ismail-spec",
+  },
+  {
+    name: "Adrio Alimin",
+    picture: adrio,
+    linkedin: "https://www.linkedin.com/in/adrio-alimin-8b5343152",
+    github: "https://github.com/lyulka",
+  },
+  {
+    name: "Eehit Ray",
+    picture: eehit,
+    linkedin: "https://www.linkedin.com/in/eehit-ray-52559b195/",
+    github: "https://github.com/greatblitz982",
   },
 ];
